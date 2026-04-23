c23f798cf2d7b9b2fde5d387d331b921f226ff9f
gaelordu7 <gaelorduna67@aragon.unam.mx>
2026-04-22 22:38:06 -0600

    hash.py
    Tablas hash

--- hash.py
+++ hash.py
@@ -1,34 +1,19 @@
 class TablaHash:
     def __init__(self, m=7):
-        """
-        Inicializa la tabla con tamaño m=7.
-        Complejidad Temporal: O(m) para crear las cubetas.
-        """
         self.m = m
         self.n = 0
         self.tabla = [[] for _ in range(self.m)]
 
     def _calcular_suma_ascii(self, cadena):
-        """
-        Calcula el valor automático sumando los códigos ASCII.
-        Complejidad Temporal: O(k), donde k es el número de caracteres de la cadena.
-        """
+
         return sum(ord(char) for char in str(cadena))
 
     def _hash(self, suma_ascii):
-        """
-        Calcula el índice de la tabla usando el método de división.
-        Complejidad Temporal: O(1).
-        """
+
         return suma_ascii % self.m
 
     def insertar(self, clave):
-        """
-        Inserta un par clave-valor. Si la clave existe, actualiza el valor.
-        Complejidad Temporal: 
-           - Promedio: O(1 + alpha), donde alpha es el factor de carga.
-           - Peor caso: O(n) si todos los elementos colisionan en la misma cubeta.
-        """
+ 
         valor_automatico = self._calcular_suma_ascii(clave)
         indice = self._hash(valor_automatico)
         
@@ -42,12 +27,7 @@ class TablaHash:
         return valor_automatico
 
     def buscar(self, clave):
-        """
-        Busca una clave y retorna el valor ASCII asociado.
-        Complejidad Temporal:
-           - Promedio: O(1 + alpha).
-           - Peor caso: O(n).
-        """
+
         suma_ascii = self._calcular_suma_ascii(clave)
         indice = self._hash(suma_ascii)
         for c, v in self.tabla[indice]:
@@ -56,12 +36,7 @@ class TablaHash:
         return None
 
     def eliminar(self, clave):
-        """
-        Elimina la clave de la tabla.
-        Complejidad Temporal:
-           - Promedio: O(1 + alpha).
-           - Peor caso: O(n).
-        """
+ 
         suma_ascii = self._calcular_suma_ascii(clave)
         indice = self._hash(suma_ascii)
         for i, (c, v) in enumerate(self.tabla[indice]):
@@ -72,18 +47,12 @@ class TablaHash:
         return False
 
     def factor_carga(self):
-        """
-        Calcula la densidad de la tabla (alpha = n/m).
-        Complejidad Temporal: O(1).
-        """
+
         return self.n / self.m
 
     def mostrar_tabla(self):
-        """
-        Visualiza la estructura de encadenamiento.
-        Complejidad Temporal: O(m + n).
-        """
-        print("\n--- VISUALIZACIÓN DE LA TABLA ---")
+
+        print("\n--- TABLA HASH ---")
         for i in range(self.m):
             nodos = [f"[{c}: {v}]" for c, v in self.tabla[i]]
             print(f"Índice {i}: {' -> '.join(nodos) if nodos else 'Vacio'}")
